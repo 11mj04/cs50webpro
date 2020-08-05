5e40df43a49f351d8d0cf6a5928537052ec19bb3
Majouran Jeganathan <majouran.jeganathan@gmail.com>
2020-08-05 22:53:35 +0200

    create and moved files into the html and python folders

---
+++ python/lists.py
@@ -0,0 +1,7 @@
+# Define a list of names 
+names = ["Harry", "Ron", "Hermoine", "Ginny"]
+
+names.append("Draco")
+names.sort()
+
+print(names)
---
+++ python/loops.py
@@ -0,0 +1,9 @@
+names = ["Harry", "Ron", "Hermoine"]
+
+for name in names:
+    print(name)
+
+name = "Harry"
+
+for character in name:
+    print(character)
